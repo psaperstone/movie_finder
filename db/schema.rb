2572d0ad96a2e473688b9a2bce80d5259377f64f
Paul Saperstone <paul@saperstone.com>
2016-04-30 14:28:27 -0400

    paul user.db updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429230508) do
+ActiveRecord::Schema.define(version: 20160430181711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20160429230508) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "movie_id"
   end
 
+  add_index "users", ["movie_id"], name: "index_users_on_movie_id", using: :btree
+
+  add_foreign_key "users", "movies"
 end
