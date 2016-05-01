88f4ba9f27c4cabd4ddae2aa4b58381678518f97
Timothy Ingram <tim.ringram@gmail.com>
2016-05-01 16:06:24 -0400

    yet again

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,34 +26,34 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.new(movie_params)
 
-   movie_id = Tmdb::Movie.find(@movie.title)[0].id
+      movie_id = Tmdb::Movie.find(@movie.title)[0].id
 
-   title_info_2 = Tmdb::Movie.detail(movie_id)
-   genre = title_info_2['genres'][0]['name']
-   vote = title_info_2['vote_average']
-   year = title_info_2['release_date'].byteslice(0,4)
+      title_info_2 = Tmdb::Movie.detail(movie_id)
+      genre = title_info_2['genres'][0]['name']
+      vote = title_info_2['vote_average']
+      year = title_info_2['release_date'].byteslice(0,4)
 
-   cast = Tmdb::Movie.casts(movie_id)
-   cast_members = []
+      cast = Tmdb::Movie.casts(movie_id)
+      cast_members = []
 
-   cast.each do |member|
-     cast_members << member['name']
-   end
+      cast.each do |member|
+      cast_members << member['name']
+      end
 
-   @movie.actors = cast_members.join(', ')
-   @movie.genre = genre
-   @movie.year = year
-   @movie.metascore = vote
-   @movie.user_id = current_user.id
+      @movie.actors = cast_members.join(', ')
+      @movie.genre = genre
+      @movie.year = year
+      @movie.metascore = vote
+      @movie.user_id = current_user.id
 
     respond_to do |format|
-      if @movie.save
-        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
-        format.json { render :show, status: :created, location: @movie }
-      else
-        format.html { render :new }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
+    if @movie.save
+      format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
+      format.json { render :show, status: :created, location: @movie }
+    else
+      format.html { render :new }
+      format.json { render json: @movie.errors, status: :unprocessable_entity }
+    end
     end
   end
 
@@ -68,30 +68,30 @@ class MoviesController < ApplicationController
 
   def film_appearances
     p @search = Tmdb::Search.new
-   new_var = @search.resource('person')                 # determines type of resource
- newest_var = @search.query('will ferrell')     # the query to search against
- actor = @search.fetch[0]['id']    # retrieve the actor/actress ID
- all_credits = Tmdb::Person.credits(actor)            # returns all credits info
+    new_var = @search.resource('person')                 # determines type of resource
+    newest_var = @search.query('will ferrell')     # the query to search against
+    actor = @search.fetch[0]['id']    # retrieve the actor/actress ID
+    all_credits = Tmdb::Person.credits(actor)            # returns all credits info
 
  # films_in = Tmdb::Person.credits(2231)['cast'][0]['title']  # iterate and capture this
  # films_in = all_credits['cast'][0]['title']  # iterate and capture this
- newer_var = all_credits['cast']
- p newer_var
+    newer_var = all_credits['cast']
+    p newer_var
 
  ### GET EACH of those ACTOR/ACRESS FILMS RATING
-   p @search = Tmdb::Search.new
-   new_rating_var = @search.resource('movie')                 # determines type of resource
-   newest_rating_var = @search.query('Step Brothers')     # the query to search against
-   rating = @search.fetch[0]['vote_average']    # retrieve the movie rating
+    p @search = Tmdb::Search.new
+    new_rating_var = @search.resource('movie')                 # determines type of resource
+    newest_rating_var = @search.query('Step Brothers')     # the query to search against
+    rating = @search.fetch[0]['vote_average']    # retrieve the movie rating
 
- @film_appearances = []
+    @film_appearances = []
 
- newer_var.each do |film|
-   @film_appearances << film['title']
- end
+    newer_var.each do |film|
+    @film_appearances << film['title']
+    end
 
- p @film_appearances.compact
- # @film_appearances = @film_appearances#.compact.join(', ')
+    p @film_appearances.compact
+    # @film_appearances = @film_appearances#.compact.join(', ')
   end
 
   # PATCH/PUT /movies/1
