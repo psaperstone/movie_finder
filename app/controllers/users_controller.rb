2bdb141d5ab8f9fa61fa011c35eee55387b60ec9
Timothy Ingram <tim.ringram@gmail.com>
2016-05-01 15:36:22 -0400

    update upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
     session[:user_id] = @user.id
-    redirect_to '/users'
+    redirect_to current_user
   else
     redirect_to '/signup'
   end
